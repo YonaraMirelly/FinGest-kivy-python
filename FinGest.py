ccc9eae83f0543dee963a3b80866fcb0e61bb8e2
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-23 20:57:06 -0300

    commit

--- FinGest.py
+++ FinGest.py
@@ -23,7 +23,7 @@ class FinGestApp(App):
         background = Image(source='FinGest_melhor.png', allow_stretch=True, keep_ratio=False)
         
         #esse botão direciona para a 2ª interface
-        start_button = Button(text='Gerir Agora!', size_hint=(0.3, 0.1),
+        start_button = Button(text='Gerir Agora!', size_hint=(0.4, 0.1),
                               font_size=45, color = (159/255.0,226/255.0,191/255.0,1),
                               pos_hint={"center_x": 0.5, "center_y": 0.1},
                               background_color=(118/255.0, 215/255.0, 196/255.0, 1),
