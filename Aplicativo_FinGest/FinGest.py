c7c4d8cc7e8d3458ae8330d77c30016c24708cd9
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-02-18 15:54:45 -0300

    commit

--- Aplicativo_FinGest/FinGest.py
+++ Aplicativo_FinGest/FinGest.py
@@ -39,7 +39,7 @@ class FinGest(App):
         start_button = Button(text='Gerir Agora!', size_hint=(0.3, 0.1),
                               font_size=45, color = (159/255.0,226/255.0,191/255.0,1),
                               pos_hint={"center_x": 0.5, "center_y": 0.2},
-                              background_color=(118/255.0, 215/255.0, 196/255.0, 1), elevation=10,
+                              background_color=(118/255.0, 215/255.0, 196/255.0, 1),
                               on_press=self.salario)
         #esse botão sai do app
         exit_button = Button(text='Sair do App', size_hint=(0.3, 0.1),
